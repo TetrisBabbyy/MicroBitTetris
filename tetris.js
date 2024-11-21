155df962f5e2128532d1d886a9961079499741af
PolarizePcsOfficial <123495627+PolarizePcsOfficial@users.noreply.github.com>
2024-11-21 16:50:17 -0700

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,34 +1,11 @@
-function clearFullColumns() {
-    for (let col3 = 0; col3 <= 4; col3++) {
-        fullColumn = true
-        for (let row2 = 0; row2 <= 4; row2++) {
-            if (!(led.point(col3, row2))) {
-                fullColumn = false
-                break;
-            }
-        }
-        if (fullColumn) {
-            for (let row3 = 0; row3 <= 4; row3++) {
-                led.unplot(col3, row3)
-            }
-            for (let d = col3; d < 4; d++) {
-                for (let s = 0; s < 5; s++) {
-                    if (led.point(d + 1, s)) {
-                        led.plot(d, s)
-                    } else {
-                        led.unplot(d, s)
-                    }
-                }
-            }
-        }
-    }
-}
-input.onButtonPressed(Button.A, function () {
-    if (x > 0) {
-        led.unplot(x, y)
-        x += 0 - 1
-    }
-})
+let fullRow = false
+let y = 0
+let fullColumn = false
+let x = 2
+let speed = 500
+basic.clearScreen()
+basic.pause(1000)
+
 function clearFullRows() {
     for (let row = 0; row <= 4; row++) {
         fullRow = true
@@ -39,6 +16,7 @@ function clearFullRows() {
             }
         }
         if (fullRow) {
+            flashRow(row) 
             for (let col2 = 0; col2 <= 4; col2++) {
                 led.unplot(col2, row)
             }
@@ -54,6 +32,67 @@ function clearFullRows() {
         }
     }
 }
+
+function flashRow(row: number) {
+    for (let i = 0; i < 3; i++) {
+        for (let col = 0; col <= 4; col++) {
+            led.plot(col, row)
+        }
+        basic.pause(100)  
+        for (let col = 0; col <= 4; col++) {
+            led.unplot(col, row)
+        }
+        basic.pause(100) 
+    }
+}
+
+function clearFullColumns() {
+    for (let col3 = 0; col3 <= 4; col3++) {
+        fullColumn = true
+        for (let row2 = 0; row2 <= 4; row2++) {
+            if (!(led.point(col3, row2))) {
+                fullColumn = false
+                break;
+            }
+        }
+        if (fullColumn) {
+            flashColumn(col3)  
+            for (let row3 = 0; row3 <= 4; row3++) {
+                led.unplot(col3, row3)
+            }
+            for (let d = col3; d < 4; d++) {
+                for (let s = 0; s < 5; s++) {
+                    if (led.point(d + 1, s)) {
+                        led.plot(d, s)
+                    } else {
+                        led.unplot(d, s)
+                    }
+                }
+            }
+        }
+    }
+}
+
+function flashColumn(col: number) {
+    for (let i = 0; i < 3; i++) {
+        for (let row = 0; row <= 4; row++) {
+            led.plot(col, row)
+        }
+        basic.pause(100)  
+        for (let row = 0; row <= 4; row++) {
+            led.unplot(col, row)
+        }
+        basic.pause(100)  
+    }
+}
+
+input.onButtonPressed(Button.A, function () {
+    if (x > 0) {
+        led.unplot(x, y)
+        x += 0 - 1
+    }
+})
+
 input.onButtonPressed(Button.B, function () {
     if (x < 4) {
         led.unplot(x, y)
@@ -61,14 +100,7 @@ input.onButtonPressed(Button.B, function () {
         led.plot(x, y)
     }
 })
-let fullRow = false
-let y = 0
-let fullColumn = false
-let x = 0
-x = 2
-let speed = 500
-basic.clearScreen()
-basic.pause(1000)
+
 basic.forever(function () {
     led.plot(x, y)
     basic.pause(speed)
@@ -83,13 +115,39 @@ basic.forever(function () {
     }
 })
 
-let melody = [
-    262, 262, 294, 349, 349, 349, 349, 349, 349, 294, 262, 262, 294, 349, 349, 349,
-    349, 349, 349, 294, 262, 262, 294, 349, 349, 349, 349, 349, 349, 294, 262
-]
-
 basic.forever(function () {
-    for (let note of melody) {
-        music.playTone(note, music.beat(BeatFraction.Half))
-    }
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Whole))
+    basic.pause(400)
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Half))
 })
